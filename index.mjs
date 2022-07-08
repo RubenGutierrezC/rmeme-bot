8c3ecb1bb4b1521ecabbcce526e4406c74f220fa
Ruben <rubdeivis@gmail.com>
2022-07-07 21:47:43 -0400

    ready to deploy

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { Telegraf } from "telegraf";
 import axios from 'axios'
 
-const bot = new Telegraf('telegram_token');
+const bot = new Telegraf('5474692629:AAGFDqDrV6uDwlUtBOHD-8BoJLN5RdX2VwA');
 
 bot.command("start", (ctx) => {
   ctx.reply('Mensaje de bienvenida')
@@ -9,7 +9,22 @@ bot.command("start", (ctx) => {
 
 bot.command("meme", async (ctx) => {
 
+  try {
 
+    const response = await axios('https://meme-api.herokuapp.com/gimme')
+
+    const data = response.data
+
+    if (data.url) {
+      ctx.replyWithPhoto(data.url || '')
+
+    } else {
+      ctx.reply("hubo un error al buscar el meme");
+    }
+  } catch (error) {
+    ctx.reply("hubo un error al buscar el meme");
+
+  }
 });
 
 bot.launch();
